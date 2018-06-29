51de46cfd82dd3ba0b8a4d3bf899b04a64ad1f71
Steven Greulich <greulich.steven@gmail.com>
2018-06-28 18:08:08 -0700

    Commit - Fit / predicted model
    The purpose of this commit is that a Linear Regression model was created, fitted and predicted against

--- BMI.py
+++ BMI.py
@@ -6,6 +6,12 @@ Created on Thu Jun 28 17:58:31 2018
 """
 
 import pandas as pd
-import numpy as np
+from sklearn.linear_model import LinearRegression
 
 bmi_life_data = pd.read_csv('bmi_and_life_expectancy.csv')
+
+model = LinearRegression()
+model.fit(bmi_life_data[['BMI']], bmi_life_data[['Life expectancy']])
+
+laos_life_exp = model.predict(21.07931 )
+print(laos_life_exp )
